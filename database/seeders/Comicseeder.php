fcec17487dedaca92a70ca5b4674da0749e2b245
Mftr96 <matteo.federici96@gmail.com>
2024-07-05 17:55:28 +0200

    risoluzione ComicSeeder(errore app con a minuscola e parentesi mancanti in Comic

--- database/seeders/Comicseeder.php
+++ database/seeders/Comicseeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use app\Models\Comic;
+use App\Models\Comic;
 //importo faker
 use Faker\Generator as Faker;
 
@@ -17,7 +17,7 @@ class Comicseeder extends Seeder
     public function run(Faker $faker): void
     {
         //creo un nuovo Comic 
-        $new_comic= new Comic;
+        $new_comic= new Comic();
         //con i relativi dati da inserire
         $new_comic->title="topolino";
         $new_comic->thumb="https://fumettirari.com/wp-content/uploads/2021/10/topolino-n-1-min-scaled.jpg";
@@ -26,7 +26,7 @@ class Comicseeder extends Seeder
         //-------------
         //creo 5 fumetti finti 
         for ($i=0; $i <5 ; $i++) { 
-            $new_comic= new Comic;
+            $new_comic= new Comic();
             $new_comic->title=$faker->sentence(2);
             $new_comic->thumb="https://picsum.photos/id/" . $faker->numberBetween(1, 700) . "/1920/1080";
             $new_comic->price=$faker->randomFloat(2, 0.99, 5.99);
